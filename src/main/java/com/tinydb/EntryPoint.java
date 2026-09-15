f282bd635a0aa76f7f012f11b30b5d3ef0eae338
hoangha5 <hoanghan0613@gmail.com>
2026-09-15 10:57:35 -0400

    scaffold

---
+++ src/main/java/com/tinydb/EntryPoint.java
@@ -0,0 +1,11 @@
+package com.tinydb;
+
+/*
+This is the entry point of the TinyDB application.
+*/
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        // TODO starts the program
+    }
+}
